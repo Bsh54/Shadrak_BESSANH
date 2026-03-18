3a06ccee01be509b1f1365f87a73911c7b900a79
Bsh54 <shadrakbsh@gmail.com>
2026-03-18 15:28:35 +0100

    Simplification calendrier GitHub
    Juste changement de couleur en bleu #2563EB

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -3,23 +3,6 @@ import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
 
 function Github() {
-  const selectLastHalfYear = contributions => {
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const shownMonths = 6;
-
-    return contributions.filter(activity => {
-      const date = new Date(activity.date);
-      const monthOfDay = date.getMonth();
-
-      return (
-        date.getFullYear() === currentYear &&
-        monthOfDay < currentMonth &&
-        monthOfDay >= currentMonth - shownMonths
-      );
-    });
-  };
-
   return (
     <Row
       style={{
@@ -32,24 +15,11 @@ function Github() {
       </h1>
       <GitHubCalendar
         username="Bsh54"
-        transformData={selectLastHalfYear}
         blockSize={15}
         blockMargin={5}
         color="#2563EB"
         fontSize={16}
-        labels={{
-          months: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aoû', 'Sep', 'Oct', 'Nov', 'Déc'],
-          weekdays: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
-          totalCount: '{{count}} contributions en {{year}}',
-          legend: {
-            less: 'Moins',
-            more: 'Plus'
-          }
-        }}
-        theme={{
-          light: ['#F3F4F6', '#BFDBFE', '#60A5FA', '#2563EB', '#1D4ED8'],
-          dark: ['#F3F4F6', '#BFDBFE', '#60A5FA', '#2563EB', '#1D4ED8']
-        }}
+        year="last"
       />
     </Row>
   );
