c6c7452484a93a5823685823de732872074d338e
Bsh54 <shadrakbsh@gmail.com>
2026-03-18 01:40:26 +0100

    Suppression de la section Qui suis-je

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
-import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/avatar.png";
 import Toolstack from "./Toolstack";
 
 function About() {
@@ -14,28 +12,6 @@ function About() {
       <Particle />
       <Container fluid className="about-section">
         <Container>
-          <Row style={{ justifyContent: "center", padding: "10px" }}>
-            <Col
-              md={7}
-              style={{
-                justifyContent: "center",
-                paddingTop: "30px",
-                paddingBottom: "50px",
-              }}
-            >
-              <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-                Qui <strong className="purple">SUIS-JE</strong>
-              </h1>
-              <Aboutcard />
-            </Col>
-            <Col
-              md={5}
-              style={{ paddingTop: "120px", paddingBottom: "50px" }}
-              className="about-img"
-            >
-              <img src={laptopImg} alt="about" className="img-fluid" />
-            </Col>
-          </Row>
           <h1 className="project-heading">
             Compétences <strong className="purple">Professionnelles</strong>
           </h1>
